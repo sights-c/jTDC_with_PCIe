a5d2dd135d0d7e7794aaa41e4c60e7b592aabe4e
sights-c <870408605@outlook.com>
2024-10-24 17:56:34 +0800

    update <dsp_multioption_counter.v> replacing DSP48A1 of spartan-6 with 7-series DSP48E1

--- modules/counter/dsp_multioption_counter.v
+++ modules/counter/dsp_multioption_counter.v
@@ -81,67 +81,94 @@ module dsp_multioption_counter #(
 
 
 	
-	DSP48A1 #(
-		.A0REG ( 0 ),
-		.A1REG ( 0 ),
-		.B0REG ( 0 ),
-		.B1REG ( 0 ),
-		.CARRYINREG ( 0 ),
-		.CARRYINSEL ( "OPMODE5" ), 
-		.CREG ( 0 ),
-		.DREG ( 0 ),
-		.MREG ( 0 ),
-		.OPMODEREG ( 0 ),
-		.PREG ( 1 ),
-		.RSTTYPE ( "SYNC" ),
-		.CARRYOUTREG ( 0 ))
-
-	DSP48A1_SLICE (
-
-		.CLK(countClock),
-	
-		//inputs
-		.A(18'b0),
-		//counter (31bit) should count in the upper range [47:17] to use the real overflow bit of the DSP
-		.B(18'b10_00000000_00000000),	
-		.C(48'b0),
-		.D(18'b0),
-
-		//CE
-		.CEA(1'b0),
-		.CEB(1'b0),
-		.CEC(1'b0),
-		.CED(1'b0),
-		.CEM(1'b0),
-		.CEP(final_count),
-		.CEOPMODE(1'b0),
-		.CECARRYIN(1'b0),
-
-		//resets
-		.RSTA(1'b0),
-		.RSTB(1'b0),
-		.RSTC(1'b0),
-		.RSTD(1'b0),
-		.RSTM(1'b0),
-		.RSTP(final_reset),
-		.RSTOPMODE(1'b0),
-		.RSTCARRYIN(1'b0),
-
-		//carry inputs
-		.CARRYIN(1'b0),
-		.PCIN(48'b0),
-
-		//outputs
-		.CARRYOUTF(CARRYOUT),	
-		.CARRYOUT(),	//unconnected
-		.BCOUT(),		//unconnected
-		.PCOUT(),		//unconnected
-		.M(),				//unconnected
-		.P(DSPOUT),
-
-		//OPMODE
-		.OPMODE({4'b0000,OPMODE_Z,OPMODE_X})
-		);
+	DSP48E1 #(
+		// Feature Control Attributes: Data Path Selection
+		.A_INPUT("DIRECT"),               // Selects A input source, "DIRECT" (A port) or "CASCADE" (ACIN port)
+		.B_INPUT("DIRECT"),               // Selects B input source, "DIRECT" (B port) or "CASCADE" (BCIN port)
+		.USE_DPORT("FALSE"),              // Select D port usage (TRUE or FALSE)
+		.USE_MULT("NONE"),            	  // Select multiplier usage ("MULTIPLY", "DYNAMIC", or "NONE")
+		.USE_SIMD("ONE48"),               // SIMD selection ("ONE48", "TWO24", "FOUR12")
+		// Pattern Detector Attributes: Pattern Detection Configuration
+		.AUTORESET_PATDET("RESET_MATCH"), // "NO_RESET", "RESET_MATCH", "RESET_NOT_MATCH" 
+		.MASK(48'h3fffffffffff),          // 48-bit mask value for pattern detect (1=ignore)
+		.PATTERN(48'h000000000000),       // 48-bit pattern match for pattern detect
+		.SEL_MASK("MASK"),                // "C", "MASK", "ROUNDING_MODE1", "ROUNDING_MODE2" 
+		.SEL_PATTERN("PATTERN"),          // Select pattern value ("PATTERN" or "C")
+		.USE_PATTERN_DETECT("NO_PATDET"), // Enable pattern detect ("PATDET" or "NO_PATDET")
+		// Register Control Attributes: Pipeline Register Configuration
+		.ACASCREG(0),                     // Number of pipeline stages between A/ACIN and ACOUT (0, 1 or 2)
+		.ADREG(0),                        // Number of pipeline stages for pre-adder (0 or 1)
+		.ALUMODEREG(0),                   // Number of pipeline stages for ALUMODE (0 or 1)
+		.AREG(0),                         // Number of pipeline stages for A (0, 1 or 2)
+		.BCASCREG(0),                     // Number of pipeline stages between B/BCIN and BCOUT (0, 1 or 2)
+		.BREG(0),                         // Number of pipeline stages for B (0, 1 or 2)
+		.CARRYINREG(0),                   // Number of pipeline stages for CARRYIN (0 or 1)
+		.CARRYINSELREG(0),                // Number of pipeline stages for CARRYINSEL (0 or 1)
+		.CREG(0),                         // Number of pipeline stages for C (0 or 1)
+		.DREG(0),                         // Number of pipeline stages for D (0 or 1)
+		.INMODEREG(0),                    // Number of pipeline stages for INMODE (0 or 1)
+		.MREG(0),                         // Number of multiplier pipeline stages (0 or 1)
+		.OPMODEREG(0),                    // Number of pipeline stages for OPMODE (0 or 1)
+		.PREG(1)                          // Number of pipeline stages for P (0 or 1)
+	 )
+	 DSP48E1_inst (
+		// Cascade: 30-bit (each) output: Cascade Ports
+		.ACOUT(),                        // 30-bit output: A port cascade output
+		.BCOUT(),                   	 // 18-bit output: B port cascade output
+		.CARRYCASCOUT(),     			 // 1-bit output: Cascade carry output
+		.MULTSIGNOUT(),       			 // 1-bit output: Multiplier sign cascade output
+		.PCOUT(),                   	 // 48-bit output: Cascade output
+		// Control: 1-bit (each) output: Control Inputs/Status Bits
+		.OVERFLOW(),             		 // 1-bit output: Overflow in add/acc output
+		.PATTERNBDETECT(), 				 // 1-bit output: Pattern bar detect output
+		.PATTERNDETECT(),   			 // 1-bit output: Pattern detect output
+		.UNDERFLOW(),           		 // 1-bit output: Underflow in add/acc output
+		// Data: 4-bit (each) output: Data Ports
+		.CARRYOUT({CARRYOUT,3'bz}),    	 // 4-bit output: Carry output
+		.P(DSPOUT),                	 	 // 48-bit output: Primary data output
+		// Cascade: 30-bit (each) input: Cascade Ports
+		.ACIN(),                     	 // 30-bit input: A cascade data input
+		.BCIN(),                     	 // 18-bit input: B cascade input
+		.CARRYCASCIN(),       			 // 1-bit input: Cascade carry input
+		.MULTSIGNIN(),         			 // 1-bit input: Multiplier sign input
+		.PCIN(),                     	 // 48-bit input: P cascade input
+		// Control: 4-bit (each) input: Control Inputs/Status Bits
+		.ALUMODE(4'b0110),               // 4-bit input: ALU control input
+		.CARRYINSEL(3'b00),         	 // 3-bit input: Carry select input
+		.CLK(countClock),              	 // 1-bit input: Clock input
+		.INMODE(),                 		 // 5-bit input: INMODE control input
+		.OPMODE({1'b0,OPMODE_Z,2'b00,OPMODE_X}),	// 7-bit input: Operation mode input
+		// Data: 30-bit (each) input: Data Ports
+		.A(30'b0),                    	 // 30-bit input: A data input
+		.B(18'b10_00000000_00000000),    // 18-bit input: B data input
+		.C(48'b0),                       // 48-bit input: C data input
+		.CARRYIN(1'b0),               	 // 1-bit input: Carry input signal
+		.D(25'b0),                       // 25-bit input: D data input
+		// Reset/Clock Enable: 1-bit (each) input: Reset/Clock Enable Inputs
+		.CEA1(),                     	 // 1-bit input: Clock enable input for 1st stage AREG
+		.CEA2(),                     	 // 1-bit input: Clock enable input for 2nd stage AREG
+		.CEAD(),                     	 // 1-bit input: Clock enable input for ADREG
+		.CEALUMODE(),           		 // 1-bit input: Clock enable input for ALUMODE
+		.CEB1(),                     	 // 1-bit input: Clock enable input for 1st stage BREG
+		.CEB2(),                     	 // 1-bit input: Clock enable input for 2nd stage BREG
+		.CEC(),                       	 // 1-bit input: Clock enable input for CREG
+		.CECARRYIN(),           		 // 1-bit input: Clock enable input for CARRYINREG
+		.CECTRL(),                 		 // 1-bit input: Clock enable input for OPMODEREG and CARRYINSELREG
+		.CED(),                       	 // 1-bit input: Clock enable input for DREG
+		.CEINMODE(),             		 // 1-bit input: Clock enable input for INMODEREG
+		.CEM(),                       	 // 1-bit input: Clock enable input for MREG
+		.CEP(final_count),           	 // 1-bit input: Clock enable input for PREG
+		.RSTA(),                     	 // 1-bit input: Reset input for AREG
+		.RSTALLCARRYIN(),   			 // 1-bit input: Reset input for CARRYINREG
+		.RSTALUMODE(),         			 // 1-bit input: Reset input for ALUMODEREG
+		.RSTB(),                     	 // 1-bit input: Reset input for BREG
+		.RSTC(),                     	 // 1-bit input: Reset input for CREG
+		.RSTCTRL(),               		 // 1-bit input: Reset input for OPMODEREG and CARRYINSELREG
+		.RSTD(),                     	 // 1-bit input: Reset input for DREG and ADREG
+		.RSTINMODE(),           		 // 1-bit input: Reset input for INMODEREG
+		.RSTM(),                     	 // 1-bit input: Reset input for MREG
+		.RSTP(final_reset)            	 // 1-bit input: Reset input for PREG
+	);
 
 
 
